9e49319d8bda5c35a34d6b70220e615eb4f0c12c
Bruno Santos <bruno.santos@pottencial.com.br>
2019-07-07 15:23:34 -0300

    Desingning a repository contract

---
+++ Books.api/Services/Interface.cs
@@ -0,0 +1,21 @@
+﻿using Books.api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.api.Services
+{
+    public interface IBooksRepository
+    {
+
+        //IEnumerable<Book> GetBooks();
+        //Book GetBook(Guid id);
+
+        Task<IEnumerable<Book>> GetBooksAsync();
+        Task<Book> GetBookAsync(Guid id);
+            
+
+
+    }
+}
